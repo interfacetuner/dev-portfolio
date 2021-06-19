6eac6ace0cf176fc12f0bbd0f793a7f7328c7994
Damian Hankins <damianchankins@gmail.com>
2021-06-18 22:06:55 -0400

    name change

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -7,7 +7,7 @@ const navBar = {
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
   firstName: "Damian",
-  middleName: "",
+  middleName: "Carl",
   lastName: "Hankins",
   message: " Passionate about changing humanitarian issues with technology. ",
   icons: [
